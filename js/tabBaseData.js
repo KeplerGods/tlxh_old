8ac3e922f0338c4186ef8a933628880c106e2bb0
AaronLi <ljz1318078262@163.com>
2018-11-22 23:19:10 +0800

    新增菜单

--- js/tabBaseData.js
+++ js/tabBaseData.js
@@ -28,24 +28,233 @@ tabsName = [{
 	childs: [{
 		id: "0",
 		show: true,
-		name: "协会领导活动"
+		name: "协会要闻"
 	}, {
 		id: "1",
 		show: true,
-		name: "协会主办活动"
+		name: "培训信息"
 	}, {
 		id: "2",
 		show: true,
-		name: "直属机构活动"
-	}, {
-		id: "3",
-		show: true,
-		name: "分支机构活动"
-	}, {
-		id: "4",
-		show: true,
-		name: "投稿排行榜"
+		name: "会展信息"
 	}]
+}, {
+	id: "201",
+	show: true,
+	name: "党建工作",
+	curl: "/appData",
+	childs: [{
+			id: "201",
+			show: true,
+			name: "党内法规"
+		},
+		{
+			id: "202",
+			show: true,
+			name: "重要文件"
+		},
+		{
+			id: "203",
+			show: true,
+			name: "领导讲话"
+		},
+		{
+			id: "204",
+			show: true,
+			name: "规定法办"
+		},
+		{
+			id: "205",
+			show: true,
+			name: "工作部署"
+		},
+		{
+			id: "206",
+			show: true,
+			name: "信息简报"
+		},
+		{
+			id: "207",
+			show: true,
+			name: "经验交流"
+		},
+		{
+			id: "208",
+			show: true,
+			name: "党组织建设"
+		},
+		{
+			id: "209",
+			show: true,
+			name: "学习制度"
+		},
+		{
+			id: "210",
+			show: true,
+			name: "集体领导制度"
+		},
+		{
+			id: "211",
+			show: true,
+			name: "党支部党员大会"
+		},
+		{
+			id: "212",
+			show: true,
+			name: "党支部委员大会"
+		},
+		{
+			id: "213",
+			show: true,
+			name: "党小组会"
+		},
+		{
+			id: "214",
+			show: true,
+			name: "民主生活会"
+		},
+		{
+			id: "215",
+			show: true,
+			name: "发展党员"
+		},
+		{
+			id: "216",
+			show: true,
+			name: "民主评议党员"
+		},
+		{
+			id: "217",
+			show: true,
+			name: "主题当日活动"
+		},
+		{
+			id: "218",
+			show: true,
+			name: "费收缴"
+		},
+		{
+			id: "219",
+			show: true,
+			name: "制度规定"
+		},
+		{
+			id: "220",
+			show: true,
+			name: "党风责任制"
+		},
+		{
+			id: "221",
+			show: true,
+			name: "信息动态"
+		},
+		{
+			id: "222",
+			show: true,
+			name: "安排部署"
+		},
+		{
+			id: "223",
+			show: true,
+			name: "报告总结"
+		},
+		{
+			id: "224",
+			show: true,
+			name: "经验交流"
+		},
+		{
+			id: "225",
+			show: true,
+			name: "党建知识"
+		},
+		{
+			id: "226",
+			show: true,
+			name: "党组织设置"
+		},
+		{
+			id: "227",
+			show: true,
+			name: "信息简报"
+		},
+		{
+			id: "228",
+			show: true,
+			name: "经验做法"
+		}
+	]
+}, {
+	id: "229",
+	show: true,
+	name: "咨询服务",
+	curl: "/appData",
+	childs: [{
+			id: "229",
+			show: true,
+			name: "规划咨询   "
+		},
+		{
+			id: "230",
+			show: true,
+			name: "工程咨询   "
+		},
+		{
+			id: "231",
+			show: true,
+			name: "管理咨询    "
+		},
+		{
+			id: "232",
+			show: true,
+			name: "政治咨询    "
+		},
+		{
+			id: "233",
+			show: true,
+			name: "项目审查评估 "
+		},
+		{
+			id: "234",
+			show: true,
+			name: "教育培训    "
+		},
+		{
+			id: "235",
+			show: true,
+			name: "国际交流    "
+		},
+		{
+			id: "236",
+			show: true,
+			name: "专家委员会  "
+		},
+		{
+			id: "237",
+			show: true,
+			name: "专家带头人  "
+		},
+		{
+			id: "238",
+			show: true,
+			name: "专家队伍    "
+		},
+		{
+			id: "239",
+			show: true,
+			name: "重大项目进展 "
+		},
+		{
+			id: "240",
+			show: true,
+			name: "重大合作项目 "
+		},
+		{
+			id: "241",
+			show: true,
+			name: "研究报告    "
+		}
+	]
 }, {
 	id: "2",
 	show: true,
@@ -72,7 +281,7 @@ tabsName = [{
 	}, {
 		id: "2",
 		show: true,
-		name: "会员推荐"
+		name: "会员简介"
 	}]
 }, {
 	id: "51",
@@ -145,7 +354,7 @@ function loadTab() {
 	var user = JSON.parse(localStorage.getItem("$state") || "{}");
 	for(i = 1; i <= tabsName.length; i++) {
 		var h = "";
-		if((!user.account ||!user.type || user.type != 1) && tabsName[i - 1].id == "71") {
+		if((!user.account || !user.type || user.type != 1) && tabsName[i - 1].id == "71") {
 			h = " mui-hidden ";
 		} else {
 			tabs.push({
